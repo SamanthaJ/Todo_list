d61e0f429176f865efee9532231c192deb01b931
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-10-02 21:28:56 -0400

    wip

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,23 @@
 class TasksController < ApplicationController
 
   def index
-    @tasks = Task.new
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def create
+    @task = Task.new(task_params)
+    if @task.save  
+      redirect_to tasks_path
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
