8927b2bd48bb440c4578bb42bfd699865700b69a
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-10-06 13:38:57 -0400

    Finishes update action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,11 +2,12 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all
+    @task = Task.new
   end
 
-  def show
-    @task = Task.find(params[:id])
-  end
+  #def show
+   # @task = Task.find(params[:id])
+   #end
   
   def create
     @task = Task.new(task_params)
@@ -15,6 +16,20 @@ class TasksController < ApplicationController
     end
   end
 
+  def edit
+  	@task =Task.find(params[:id])
+  end
+
+  def update
+  	@task = Task.find(params[:id])
+ 		if @task.update(task_params)
+    
+    redirect_to tasks_path
+  else
+    render 'edit'
+  end
+end
+    
   def destroy
     @task = Task.find(params[:id])
     if @task.destroy
